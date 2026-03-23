d40700f78bb1cfee8dad0a521bb9dada66830346
Dilip Parmar <dilipparmar@gmail.com>
2026-03-23 08:16:39 +0530

    Fix UK Clarity loading and CSP

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -12,20 +12,21 @@ const CLARITY_ID = process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID || 'vzlh3nm8uo';
 
 export function injectTrackingScripts() {
   if (typeof document === 'undefined') return;
-  if (document.getElementById('wa-gtm')) return;
 
   // Google Tag Manager
-  const gtm = document.createElement('script');
-  gtm.id = 'wa-gtm';
-  gtm.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  if (!document.getElementById('wa-gtm')) {
+    const gtm = document.createElement('script');
+    gtm.id = 'wa-gtm';
+    gtm.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${GTM_ID}');`;
-  document.head.appendChild(gtm);
+    document.head.appendChild(gtm);
+  }
 
   // Google Ads / GA4
-  if (GA_ID || ADS_ID) {
+  if ((GA_ID || ADS_ID) && !document.getElementById('wa-gtag-js')) {
     const gtagJs = document.createElement('script');
     gtagJs.id = 'wa-gtag-js';
     gtagJs.async = true;
